7c5e73bb1362267ff0ee2d1b6db4e41c1b76b49e
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2023-11-01 09:31:26 -0400

    Update atividade1.py

--- atividade dia 01.11/atividade1.py
+++ atividade dia 01.11/atividade1.py
@@ -1,48 +1,32 @@
 import os
 import json
 
-if os.path.exists('notas.json'):
-    f = open('notas.json','r')
-    texto = f.read()
-    f.close()
-    notas = json.loads(texto)
-else:
-    print("Arquivo Inexistente!!")
+def carregar_tabela():
+    with open('notas.json','r') as f:
+        texto = f.read()
+        notas = json.loads(texto)
+    return notas
 
-soma=0
-cont=0
-print("------------------ TURMA 1A -------------")
-print("|{:<25} |{:<5} |{:<5}|".format("NOME","TURMA","NOTA"))
+def print_table(tabela,turma):
+    print(f"------------------ TURMA {turma} -------------")
+    print("|{:<25} |{:<5} |{:<5}|".format("NOME","TURMA","NOTA"))
 
-for i in notas:
-    if i[1] == '1A':
-        print("|{:<25} |{:<5} |{:<5}|".format(i[0],i[1],i[2]))
-        soma+=i[2]
-        cont+=1
-print(f"Média da turma: {soma/cont}")
+    for nome_reg,turma_reg,nota_reg in tabela:
+        if turma_reg == turma:
+            print("|{:<25} |{:<5} |{:<5}|".format(nome_reg,turma_reg,nota_reg))
 
-print("\n\n")
+def calcular_media(tabela,turma):
+    soma = 0
+    cont = 0
 
-soma=0
-cont=0
-print("------------------ TURMA 1B -------------")
-print("|{:<25} |{:<5} |{:<5}|".format("NOME","TURMA","NOTA"))
-for i in notas:
-    if i[1] == '1B':
-        print("|{:<25} |{:<5} |{:<5}|".format(i[0],i[1],i[2]))
-        soma+=i[2]
-        cont+=1
-print(f"Média da turma: {soma/cont}")
+    for nome_reg,turma_reg,nota_reg in tabela:
+        if turma_reg == turma:
+            soma +=nota_reg
+            cont +=1
+    return soma/cont
 
-print("\n\n")
-
-soma=0
-cont=0
-print("------------------ TURMA 1C -------------")
-print("|{:<25} |{:<5} |{:<5}|".format("NOME","TURMA","NOTA"))
-for i in notas:
-    if i[1] == '1C':
-        print("|{:<25} |{:<5} |{:<5}|".format(i[0],i[1],i[2]))
-        soma+=i[2]
-        cont+=1
-print(f"Média da turma: {soma/cont}")
+if __name__=='__main__':
+    tabela = carregar_tabela()
+    for turma in ['1A','1B','1C']:
+        print_table(tabela,turma)
+        print(f"Media da turma: {calcular_media(tabela,turma)}")
